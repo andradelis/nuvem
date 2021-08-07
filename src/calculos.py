51ee11311f7da541e71c0b11f4b41a360a1f7f3f
andradelis <andradelis@id.uff.br>
2021-08-07 12:51:34 -0300

    :rocket: cálculo de mlt adicionado

--- src/calculos.py
+++ src/calculos.py
@@ -1,6 +1,22 @@
 import pandas as pd
 import numpy as np
 
+def mlt(serie: pd.Series) -> pd.Series:
+    """
+    Obtenção da média mensal de longo termo de uma série de dados.
+    
+    Parameters
+    ----------
+    serie : pd.Series
+        Série de vazões do posto.
+        
+    Returns
+    -------
+    pd.Series
+        Série de médias de vazão por mês do posto.    
+    """
+    return serie.groupby(serie.index.month).mean()
+
 def curva_chave(cotas: pd.Series, vazoes: pd.Series, grau: int = 2) -> np.ndarray:
     """
     Determinação dos coeficientes da curva chave de um rio, a partir das séries de cotas e vazões.
@@ -22,7 +38,6 @@ def curva_chave(cotas: pd.Series, vazoes: pd.Series, grau: int = 2) -> np.ndarra
             1. array de valores no eixo y da função que melhor traduz a curva chave de um rio;
             2. coeficientes do polinômio de regressão.
     """
-    
     x = np.array(cotas)
     y = np.array(vazoes)
 
